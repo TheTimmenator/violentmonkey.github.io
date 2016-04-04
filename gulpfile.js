43366c800046cb5e18d15d559a8a241db0accdf9
Gerald <gera2ld@163.com>
2016-04-04 11:19:48 +0800

    update localization.jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 const gulp = require('gulp');
 const jade = require('gulp-jade');
+const less = require('gulp-less');
+const cssnano = require('gulp-cssnano');
 
-gulp.task('build', () => (
-  gulp.src('src/**/*.jade')
+gulp.task('build-jade', () => (
+  gulp.src('src/*.jade')
   .pipe(jade())
   .pipe(gulp.dest('dist'))
 ));
 
+gulp.task('build-css', () => (
+  gulp.src('src/*.less')
+  .pipe(less())
+  .pipe(cssnano())
+  .pipe(gulp.dest('dist'))
+));
+
+gulp.task('build', ['build-jade', 'build-css']);
+
 gulp.task('watch', () => {
-  gulp.watch('src/**/*.jade', ['build']);
+  gulp.watch('src/**/*.jade', ['build-jade']);
+  gulp.watch('src/**/*.less', ['build-css']);
 });
